0e92b788c4437986640cfa607f2dc1c9bd704cd3
Paul Madden <p.madden90@gmail.com>
2020-01-09 12:16:44 +0000

    Added PORT Int due to Heroku H10 error

--- static/app.py
+++ static/app.py
@@ -9,6 +9,5 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
-        port=int(os.environ.get('PORT')),
+        port=int(os.environ.get('PORT', 5000)),
         debug=True)
-    
