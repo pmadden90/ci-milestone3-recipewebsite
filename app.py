a76632cb47810c17636d93e57e7aa88a38db6430
Paul Madden <p.madden90@gmail.com>
2020-01-09 16:28:45 +0000

    Fixing issue with environment variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -8,12 +8,17 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'recipes_db'
+MONGODB_URI = os.environ.get("MONGO_PM_MONGO")
+DBS_NAME = "recipes_db"
+COLLECTION_NAME = "desserts"
+app.config["MONGO_URI"] = os.getenv("MONGO_PM_MONGO")
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MONGODB_NAME"] = 'recipes_db'
+
+os.environ["MONGO_PM_MONGO"] = 'mongodb+srv://root:recipespc90@myfirstcluster-8qt4w.mongodb.net/recipes_db?retryWrites=true&w=majority'
