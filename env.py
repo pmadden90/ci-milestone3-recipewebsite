0320f1056dfe4dd6f9cee2c04b8e3572d791bbaf
Paul Callaghan <43728192+pmadden90@users.noreply.github.com>
2020-01-10 18:57:34 +0000

    Update env.py

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["MONGODB_NAME"] = 'recipes_db'
 
-os.environ["MONGO_PM_MONGO"] = 'mongodb+srv://root:recipespc90@myfirstcluster-8qt4w.mongodb.net/recipes_db?retryWrites=true&w=majority'
+os.environ["MONGO_PM_MONGO"] = 'mongodb+srv://user:password@myfirstcluster-8qt4w.mongodb.net/recipes_db?retryWrites=true&w=majority'
